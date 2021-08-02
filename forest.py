ccf64dec81aec81988a80628270e9dfae35533d1
charleshrain <55643161+charleshrain@users.noreply.github.com>
2021-08-02 16:31:25 -0400

    Fixed error in race scraper

--- forest.py
+++ forest.py
@@ -22,86 +22,29 @@ class forest:
             dst = races['distance'][i]
             strt = races['start'][i]
 
-            q = "select * from v75flat where division = '" + cl + \
+            q = "select  won, winperccurrent, winsperc, placep, mintid, verklspar, case when framspar = 'FS' then 1 else 0 end as framspar, ownerwinperc, moneyrank, streckrank, betperc, trainerwinperc, pointsperc, jockeyrank  from v75flat where division = '" + cl + \
                 "' and distans = '" + dst + "' and startsatt = '" + strt + "'"
 
             table = pd.read_sql(q, connection)
+            
+            table.dropna
 
             x_train, x_test, y_train, y_test = train_test_split(
-                table.loc[:, table.columns != ['won', 'datum'], table['won'], test_size=0.25, random_state=42, stratify=table['won'])
-            
+                table.loc[:, table.columns != 'won'], table['won'], test_size=0.25, random_state=42, stratify=table['won'])
+
             n_estimators = [100, 300, 500, 800, 1200]
             max_depth = [5, 8, 15, 25, 30]
             min_samples_split = [2, 5, 10, 15, 100]
-            min_samples_leaf = [1, 2, 5, 10] 
+            min_samples_leaf = [1, 2, 5, 10]
 
-            hyperF = dict(n_estimators = n_estimators, max_depth = max_depth,  
-                        min_samples_split = min_samples_split, 
-                        min_samples_leaf = min_samples_leaf)
-            
-            forest = RandomForestClassifier(random_state = 1, n_estimators = 10, min_samples_split = 1)
+            hyperF = dict(n_estimators=n_estimators, max_depth=max_depth,
+                          min_samples_split=min_samples_split,
+                          min_samples_leaf=min_samples_leaf)
 
-            gridF = GridSearchCV(forest, hyperF, cv = 3, verbose = 1, 
-                                n_jobs = -1)
-            
+            forest = RandomForestClassifier(
+                random_state=1, n_estimators=10, min_samples_split=1)
+
+            gridF = GridSearchCV(forest, hyperF, cv=3, verbose=1,
+                                 n_jobs=-1)
+
             bestF = gridF.fit(x_train, y_train)
-            
-            
-
-            # param_grid = [
-            #     {'n_estimators': [10, 25], 'max_features': [5, 10],
-            #      'max_depth': [10, 50, None], 'bootstrap': [True, False]}
-            # ]
-
-            # upsampling, it is agued this should only be used on the traning set (change?):
-            # https://datascience.stackexchange.com/questions/15630/train-test-split-after-performing-smote
-            # table_majority = table[table.won==0]
-            # table_minority = table[table.won==1]
-
-            # table_minority_upsampled = resample(table_minority,
-            #                      replace=True,     # sample with replacement
-            #                      n_samples=len(table_majority.index),    # to match majority class
-            #                      random_state=123) # reproducible results
-
-            # table_upsampled = pd.concat([table_majority, table_minority_upsampled])
-            # print(table_minority_upsampled)
-
-            # train_test_split(X, y, random_state=0, stratify=y, shuffle=True)
-
-            # classifier = RandomForestClassifier(n_estimators = 100, random_state = 0)
-            # classifier.fit(table_upsampled['won'].reshape(-1, 1),table_upsampled['winperccurrent'].reshape(-1, 1))
-
-            # # Number of trees in random forest
-            # n_estimators = [int(x) for x in np.linspace(
-            #     start=200, stop=2000, num=10)]
-            # # Number of features to consider at every split
-            # max_features = ['auto', 'sqrt']
-            # # Maximum number of levels in tree
-            # max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
-            # max_depth.append(None)
-            # # Minimum number of samples required to split a node
-            # min_samples_split = [2, 5, 10]
-            # # Minimum number of samples required at each leaf node
-            # min_samples_leaf = [1, 2, 4]
-            # # Method of selecting samples for training each tree
-            # bootstrap = [True, False]
-
-            # random_grid = {'n_estimators': n_estimators,
-            #                'max_features': max_features,
-            #                'max_depth': max_depth,
-            #                'min_samples_split': min_samples_split,
-            #                'min_samples_leaf': min_samples_leaf,
-            #                'bootstrap': bootstrap}
-
-            # print(random_grid)
-
-            # # Use the random grid to search for best hyperparameters
-            # # First create the base model to tune
-            # rf = RandomForestClassifier()
-            # # Random search of parameters, using 3 fold cross validation,
-            # # search across 100 different combinations, and use all available cores
-            # rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
-            # # Fit the random search model
-            # rf_random.fit(train_features, train_labels)
-
-            # rf_random.best_params_
--- racewebscraper.py
+++ racewebscraper.py
@@ -5,8 +5,9 @@ import lxml
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
+
 class racescraper:
-    
+
     @classmethod
     def racescrape(cls):
 
@@ -18,101 +19,97 @@ class racescraper:
             ChromeDriverManager().install(), chrome_options=options)
 
         driver.get('https://www.atg.se/spel/V75')
-        driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]').click()
+        driver.find_element_by_xpath(
+            '//*[@id="onetrust-accept-btn-handler"]').click()
         driver.refresh()
         driver.maximize_window()
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
 
         def decide_class(data):
 
             data = str(data).lower().replace(" ", "")
 
             classes = {'bronsdivisionen': 'B',
-                    'dubbelklasslopp': 'D',
-                    'elitlopp': 'E',
-                    'flerklasslopp': 'F',
-                    'gulddivisionen': 'G',
-                    'kallblod': 'K',
-                    'klass1': '1',
-                    'klassi,': '1',
-                    'klass2': '2',
-                    'klassii,': '2',
-                    'rlingslopp': 'L',
-                    'silverdivisionen': 'S',
-                    'stodivisionen': 'Q',
-                    'stoeliten': 'X',
-                    'diamant': 'X',
-                    'utomv5': 'U',
-                    'Vv5lopp': 'V'}
+                       'dubbelklasslopp': 'D',
+                       'elitlopp': 'E',
+                       'flerklasslopp': 'F',
+                       'gulddivisionen': 'G',
+                       'kallblod': 'K',
+                       'klass1': '1',
+                       'klassi,': '1',
+                       'klass2': '2',
+                       'klassii,': '2',
+                       'rlingslopp': 'L',
+                       'silverdivisionen': 'S',
+                       'stodivisionen': 'Q',
+                       'stoeliten': 'X',
+                       'diamant': 'X',
+                       'utomv5': 'U',
+                       'Vv5lopp': 'V'}
 
             for classkey, symbol in classes.items():
                 if classkey in data:
                     return symbol
-                # elif classkey not in data:
-                #     return 'S'
-
+            return 'B'
 
         def decide_distance(data):
 
             data = str(data).lower().replace(" ", "")
 
             distances = {'1640': 'K',
-                        '1650': 'K',
-                        '1609': 'K',
-                        '2100': 'M',
-                        '2140': 'M',
-                        '2175': 'M',
-                        '2300': 'M',
-                        '2550': 'L',
-                        '2609': 'L',
-                        '2640': 'L',
-                        '2700': 'S',
-                        '2850': 'S',
-                        '3140': 'S',
-                        '3160': 'S',
-                        '4000': 'S'}
+                         '1650': 'K',
+                         '1609': 'K',
+                         '2100': 'M',
+                         '2140': 'M',
+                         '2175': 'M',
+                         '2300': 'M',
+                         '2550': 'L',
+                         '2609': 'L',
+                         '2640': 'L',
+                         '2700': 'S',
+                         '2850': 'S',
+                         '3140': 'S',
+                         '3160': 'S',
+                         '4000': 'S'}
 
             for distkey, symbol in distances.items():
                 if distkey in data:
-                    return symbol
-
+                    return symbol if symbol is not None else 'M'
 
         def decide_startmode(data):
             data = str(data).lower().replace(" ", "")
 
             startmode = {'autostart': 'A',
-                        'voltstart': 'V'}
+                         'voltstart': 'V'}
 
             for startkey, symbol in startmode.items():
                 if startkey in data:
                     return symbol
 
-
         races = pd.DataFrame(columns=['race', 'class', 'distance', 'start'])
-        
-        
+
         #  race 1 to 6
-        for i in range(1, 7):
+        for i in range(1, 8):
             klass = decide_class(driver.find_element_by_xpath(
                 '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div['+str(i)+']/div/div/div[1]/div/div[1]/div/div[2]/div[1]/span[3]').get_attribute("innerHTML"))
-            
+
             distans = decide_distance(driver.find_element_by_xpath(
                 '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div['+str(i)+']/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div[2]/span').get_attribute("innerHTML"))
-           
+
             startmode = decide_startmode(driver.find_element_by_xpath(
                 '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div['+str(i)+']/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div[2]/span').get_attribute("innerHTML"))
             races.loc[i] = [i, klass, distans, startmode]
-            
-        # race 7 (change to lambda)
-        klass = decide_class(driver.find_element_by_xpath(
-            '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[7]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/span[3]').get_attribute("innerHTML"))
-        distans = decide_distance(driver.find_element_by_xpath(
-            '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[7]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[2]/div[2]/span').get_attribute("innerHTML"))
-        startmode = decide_startmode(driver.find_element_by_xpath(
-            '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[7]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[2]/div[2]/span').get_attribute("innerHTML"))
-        races.loc[7] = [7, klass, distans, startmode]
-        
+
+        # # race 7 (change to lambda)
+        # klass = decide_class(driver.find_element_by_xpath(
+        #     '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[7]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/span[3]').get_attribute("innerHTML"))
+        #      //*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[7]/div/div/div[1]/div/div[1]/div/div[2]/div[1]/span[3]
+        # distans = decide_distance(driver.find_element_by_xpath(
+        #     '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[7]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[2]/div[2]/span').get_attribute("innerHTML"))
+        # startmode = decide_startmode(driver.find_element_by_xpath(
+        #     '//*[@id="main"]/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[7]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[2]/div[2]/span').get_attribute("innerHTML"))
+        # races.loc[7] = [7, klass, distans, startmode]
 
         driver.quit()
         return races
