8416e8e406cd75a56227b3e85fc3bc6f62e78d29
charleshrain <55643161+charleshrain@users.noreply.github.com>
2022-06-05 15:20:24 -0400

    replaced xpath with css selector in webscraper

--- webscraper.py
+++ webscraper.py
@@ -56,31 +56,31 @@ class WebScraper:
 
         # get page pertaining to correct race type
         driver.get('https://www.atg.se/spel/V75')
-        driver.find_element_by_xpath(
-            '//*[@id="onetrust-accept-btn-handler"]').click()  # cookies popup
+        driver.find_element_by_css_selector('#onetrust-accept-btn-handler').click()  # cookies popup
         driver.refresh()
         driver.maximize_window()
         time.sleep(10)
-        driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/div[5]/div/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[1]/div[1]/div/div/div[1]/div[2]/div/div/button[2]').click()
+
+        driver.find_element_by_css_selector('#main > div.active-game-type-v75 > div:nth-child(2) > div > div > div > div > div > div > div:nth-child(7) > div:nth-child(1) > div > div > div > div.flexboxgrid2_row_1w > div.flexboxgrid2_col-xs-12_2A.flexboxgrid2_col-sm-5_2S.flexboxgrid2_col-md-6_1m.startlist-header__table-wrapper.startlist-actions > div > button.css-eugx3a-startlistoptionsview-styles--configButton-Button--buttonComponent').click()
+        # driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/div[5]/div/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[1]/div[1]/div/div/div[1]/div[2]/div/div/button[2]').click()
       
         # check checkboxes for data selection and save selection
-        clear_chk = '/html/body/div[4]/div/div/div/div/div/div[2]/div/div[3]/button[1]'
-        driver.find_element_by_xpath(clear_chk).click()
+        driver.find_element_by_css_selector('body > div.modal-container > div > div > div > div > div > div.modal__body.css-krjfgr-StartlistOptionsModal-styles--optionsModalBody-Modal--Modal > div > div.css-dr5zvn-StartlistDisplayOptionsDialog-styles--displayOptionsDialogClear > button.css-tqseha-Button-styles--root-Button--Button').click()
         
-        pengar_chk = '/html/body/div[4]/div/div/div/div/div/div[2]/div/div[1]/ul/li[1]/div/span[1]'
-        driver.find_element_by_xpath(pengar_chk).click()
-        winp_chk = '/html/body/div[4]/div/div/div/div/div/div[2]/div/div[1]/ul/li[2]/div/span[1]'
-        driver.find_element_by_xpath(winp_chk).click()
-        placep_chk = '/html/body/div[4]/div/div/div/div/div/div[2]/div/div[2]/ul/li[2]/div/span[1]'
-        driver.find_element_by_xpath(placep_chk).click()
-        points_chk = '/html/body/div[4]/div/div/div/div/div/div[2]/div/div[2]/ul/li[4]/div/span[1]'
-        driver.find_element_by_xpath(points_chk).click()
-        starts_cur = '/html/body/div[4]/div/div/div/div/div/div[2]/div/div[2]/ul/li[7]/div/span[1]' #test
-        driver.find_element_by_xpath(starts_cur).click() # test
+        pengar_chk = 'body > div.modal-container > div > div > div > div > div > div.modal__body.css-krjfgr-StartlistOptionsModal-styles--optionsModalBody-Modal--Modal > div > div.col-xs-6.css-1xikekk-StartlistDisplayOptionsDialog-styles--displayOptionsColumn-StartlistDisplayOptionsDialog-styles--displayOptionsDialogPopular > ul > li:nth-child(1) > div > span.css-1ract64-Checkbox-styles--icon'
+        driver.find_element_by_css_selector(pengar_chk).click()
+        winp_chk = 'body > div.modal-container > div > div > div > div > div > div.modal__body.css-krjfgr-StartlistOptionsModal-styles--optionsModalBody-Modal--Modal > div > div.col-xs-6.css-1xikekk-StartlistDisplayOptionsDialog-styles--displayOptionsColumn-StartlistDisplayOptionsDialog-styles--displayOptionsDialogPopular > ul > li:nth-child(2) > div > span.css-1ract64-Checkbox-styles--icon'
+        driver.find_element_by_css_selector(winp_chk).click()
+        placep_chk = 'body > div.modal-container > div > div > div > div > div > div.modal__body.css-krjfgr-StartlistOptionsModal-styles--optionsModalBody-Modal--Modal > div > div.col-xs-6.css-13rqg9x-StartlistDisplayOptionsDialog-styles--displayOptionsColumn-StartlistDisplayOptionsDialog-styles--displayOptionsDialogOthers > ul > li:nth-child(2) > div > span.css-1ract64-Checkbox-styles--icon'
+        driver.find_element_by_css_selector(placep_chk).click()
+        points_chk = 'body > div.modal-container > div > div > div > div > div > div.modal__body.css-krjfgr-StartlistOptionsModal-styles--optionsModalBody-Modal--Modal > div > div.col-xs-6.css-13rqg9x-StartlistDisplayOptionsDialog-styles--displayOptionsColumn-StartlistDisplayOptionsDialog-styles--displayOptionsDialogOthers > ul > li:nth-child(4) > div > span.css-1ract64-Checkbox-styles--icon'
+        driver.find_element_by_css_selector(points_chk).click()
+        starts_cur = 'body > div.modal-container > div > div > div > div > div > div.modal__body.css-krjfgr-StartlistOptionsModal-styles--optionsModalBody-Modal--Modal > div > div.col-xs-6.css-13rqg9x-StartlistDisplayOptionsDialog-styles--displayOptionsColumn-StartlistDisplayOptionsDialog-styles--displayOptionsDialogOthers > ul > li:nth-child(6) > div > span.css-1ract64-Checkbox-styles--icon' #test
+        driver.find_element_by_css_selector(starts_cur).click() # test
 
 
-        driver.find_element_by_xpath(
-            '/html/body/div[4]/div/div/div/div/div/div[2]/div/div[3]/button[2]').click()  
+        driver.find_element_by_css_selector(
+            'body > div.modal-container > div > div > div > div > div > div.modal__body.css-krjfgr-StartlistOptionsModal-styles--optionsModalBody-Modal--Modal > div > div.css-dr5zvn-StartlistDisplayOptionsDialog-styles--displayOptionsDialogClear > button.css-1ixvue2-Button-styles--root-PrimaryButton-styles--root-StartlistDisplayOptionsDialog-styles--saveButton-PrimaryButton--PrimaryButton-StartlistDisplayOptionsDialog-styles--saveButton').click()  
     
 
         # read upcoming 7 races data into dataframe
@@ -88,9 +88,8 @@ class WebScraper:
 
         for i in range(1, 8):
 
-            df = pd.read_html(driver.find_element_by_xpath(
-                '/html/body/div[2]/div[1]/div[3]/div[5]/div/div[3]/div[2]/div/div/div/div/div/div[2]/div[6]/div[' + str(i) +  ']/div[1]/div/table').get_attribute("outerHTML"))
-                
+            df = pd.read_html(driver.find_element_by_css_selector( '#main > div.active-game-type-v75 > div:nth-child(2) > div > div > div > div > div > div > div:nth-child(7) > div:nth-child('+ str(i) + ') > div > div > table').get_attribute("outerHTML"))
+                 
             df0 = df[0]
             df0['Lopp'] = i
             df0 = df0[~df0['Kusk'].str.contains("Tillägg")]
