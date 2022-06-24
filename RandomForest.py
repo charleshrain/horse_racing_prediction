ba28fce5d8f84acbd31dc1bd7971d1244200e630
charleshrain <55643161+charleshrain@users.noreply.github.com>
2022-06-23 20:03:22 -0400

    accounted for low query returns

--- RandomForest.py
+++ RandomForest.py
@@ -27,11 +27,13 @@ class RandomForest:
             dst = races['distance'][i]
             strt = races['start'][i]
 
-            q = "select a.won, a.track, a.winp, a.placep, a.betp, a.points, a.money, a.wincur from trav.flat_temp a where a.division LIKE {}  and a.distance = {} and a.startmode = {}  and a.v75 IN ('1', '2', '3', '4', '5', '6', '7')    ".format(cl, dst, strt)
-
-            q = str(q)
+            q = "select a.won, a.track, a.winp, a.placep, a.betp, a.points, a.money, a.wincur from trav.flat_temp a where a.division LIKE {}  and a.distance = {} and a.startmode = {}  and a.v75 IN ('1', '2', '3', '4', '5', '6', '7') ".format(cl, dst, strt)
 
             table = pd.read_sql(q, connection)
+
+            if table.empty or len(table.index) < 50:
+                q = "select a.won, a.track, a.winp, a.placep, a.betp, a.points, a.money, a.wincur from trav.flat_temp a where  a.distance = {} and a.startmode = {}  and a.v75 IN ('1', '2', '3', '4', '5', '6', '7')  ".format(dst, strt)
+                table = pd.read_sql(q, connection)
             
             table = table.dropna()
 
