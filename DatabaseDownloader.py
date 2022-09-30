d607e052346f58bd7b533041f9c179b5db9b6b22
charleshrain <55643161+charleshrain@users.noreply.github.com>
2022-09-30 16:00:36 -0400

    fixed download

--- DatabaseDownloader.py
+++ DatabaseDownloader.py
@@ -6,26 +6,27 @@ import http
 import requests
 import gzip
 import zipfile
+import shutil
 
 class Downloader:
               
     @classmethod 
     def clean_downloads(cls):
         print("Cleaning...\n")
-        if os.path.exists('db-docker.gz'):
-            os.remove('db-docker.gz')
-
-        if os.path.exists('data/flat.csv'):
-            os.remove('docker-trav-image')
+        if os.path.exists('dump.sql.gz'):
+            os.remove('dump.sql.gz')
+        
+        if os.path.exists('dump.sql'):
+            os.remove('dump.sql')
 
     @classmethod 
     def download_s3_csv(cls):
         print("Downloading...\n")
         try:
-            url = "https://trottingproject.s3.ca-central-1.amazonaws.com/docker-portgres-trav-container.gz"
+            url = "https://trottingproject.s3.ca-central-1.amazonaws.com/dump.sql.gz"
             r = requests.get(url)
 
-            with open("data.zip",'wb') as f:
+            with open("dump.sql.gz",'wb') as f:
                 f.write(r.content)        
         except:
             print("An error occured when downloading")
@@ -34,13 +35,10 @@ class Downloader:
     @classmethod
     def extract_zip(cls):
     
-        zip_file = "data.zip"
+        zip_file = "dump.sql.gz"
  
-        try:
-            with zipfile.ZipFile(zip_file) as z:
-                z.extractall()
-                print("Finished extracting")
-        except:
-            print("Invalid file")
+        with gzip.open(zip_file, 'rb') as s_file, \
+                open("dump.sql", 'wb') as d_file:
+            shutil.copyfileobj(s_file, d_file, 1024)
 
                 
--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,19 @@
 This app predicts the outcome of future v75 (https://www.atg.se/spel/V75) trotting races, using a hyper-parameter tuned random forest model built on historical race data.
 
 Instructions:
-Run python3 -m pip install -r requirements.txt 
-Run 'docker-compose up'
-Create database 'trav' in docker container postgres database
-Run main.py
+Install dependencies:
+    python3 -m pip install -r requirements.txt to insta
+Run Docker container:
+    docker-compose up
+With running docker container, create database 'trav' in docker container postgres database
+    login to container shell:
+        docker exec -it <pg_container_name> /bin/bash
+    login to postres:
+        psql -U postgres
+    create datbase:
+        create database trav;
+    then exit container shell
+Import unzipped postgres dump into database:
+    docker exec -i <container name> /bin/bash -c "PGPASSWORD=postgres psql --username postgres  testing" < <local apath to unzipped dump.sql>
+
+Run main.py to start program
